84c9f59c7aaddbde35f6cd28af98ac6b41ba7a44
Samuel Cabell <fx7197@wayne.edu>
2018-05-02 12:43:56 -0400

    Updated for new api limit

--- CircleHeatmapAnalysis/Program.cs
+++ CircleHeatmapAnalysis/Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 using Newtonsoft.Json;
 using Pubg.Net;
 using System.Net;
+using System.Diagnostics;
 
 namespace CircleHeatmapAnalysis
 {
@@ -46,16 +47,28 @@ namespace CircleHeatmapAnalysis
 
             List<Point> MiramarPoints = new List<Point>();
             List<Point> ErangelPoints = new List<Point>();
-
+            
+            // Start at
             int i = 0;
-            while ( i < 100 )
+            // Take this per loop
+            int TakeNum = 50;
+            // Go until this record
+            int GoUntil = 250;
+
+            while ( i < GoUntil )
             {
-                Parallel.ForEach(
-                    matches.data.relationships.matches.data.Skip( i ).Take( 10 ),
+                Parallel.ForEach( 
+                    // Skip the first i that have already been taken, and take the next TakeNum amount
+                    matches.data.relationships.matches.data.Skip( i ).Take( TakeNum ),
                     new ParallelOptions { MaxDegreeOfParallelism = 10 }, m =>
                     {
+                        // Get the match to find the telemetry link
                         match = matchService.GetMatch( PubgRegion.PCNorthAmerica, m.id );
+
+                        // Get the telemetry data from the match
                         telemetryData = telemetryService.GetTelemetry( PubgRegion.PCNorthAmerica, match.Assets.FirstOrDefault() );
+
+                        // Find the last game state to record the circle position
                         LastGameState = telemetryData.OfType<LogGameStatePeriodic>().OrderBy( x => x.GameState.SafetyZoneRadius ).FirstOrDefault();
 
                         if( match.MapName.ToString().ToLower() == "miramar" )
@@ -63,9 +76,9 @@ namespace CircleHeatmapAnalysis
                         else if ( match.MapName.ToString().ToLower() == "erangel" )
                             ErangelPoints.Add( new Point { X = LastGameState.GameState.SafetyZonePosition.X, Y = LastGameState.GameState.SafetyZonePosition.Y } );
                     } );
-                Console.WriteLine( i + " to " + (i + 10) + " DONE" );
-                i += 10;
-                System.Threading.Thread.Sleep(61000);
+                Console.WriteLine( i + " to " + (i + TakeNum ) + " DONE" );
+                i += TakeNum;
+                System.Threading.Thread.Sleep( 61000 );              
             }
 
             string MiramarJson = JsonConvert.SerializeObject( MiramarPoints );
